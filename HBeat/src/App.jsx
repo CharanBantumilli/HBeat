eecd4d349f32766bedf1c5ffa7ed45ab817e6a7c
Charan.B <79103683+CharanBantumilli@users.noreply.github.com>
2024-09-01 15:21:03 +0000

    Main

---
+++ HBeat/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import HeartRateMonitor from '/workspaces/LOGIC_LABS/HBeat/src/HeartRateMonitor.jsx';
+
+function App() {
+  return (
+    <div className="App">
+      <HeartRateMonitor />
+    </div>
+  );
+}
+
+export default App;
+
